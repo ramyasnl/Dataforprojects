9fd77557db0e30e94a0bd37fb6eca1a613a9008b
Ramya S Manivannan <76931568+ramyasnl@users.noreply.github.com>
2022-03-06 14:39:17 -0600

    Create order.py

---
+++ Python_Projects/Module1/order.py
@@ -0,0 +1,7 @@
+# print('This is your Order')
+# def my_orderfunction():
+#   return(print("Hello from my order function"))
+class order():
+    def __init__(self):
+        print("order is ")
+        
