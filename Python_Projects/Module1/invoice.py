2decc19a1e9d5e943ac648653a2f4b94a3875b7d
Ramya S Manivannan <76931568+ramyasnl@users.noreply.github.com>
2022-03-06 14:33:36 -0600

    Create invoice.py

---
+++ Python_Projects/Module1/invoice.py
@@ -0,0 +1,8 @@
+print('This is generating Invoice')
+# def my_invoicefunction():
+#   return (print("Hello from my invoice function"))
+class invoice():
+    def __init__(self):
+        print("The Invoice is ")
+    
+ 
