f97851235cb6f2227c498e25a9e44d1384c3349f
Ramya S Manivannan <76931568+ramyasnl@users.noreply.github.com>
2022-03-06 14:45:43 -0600

    Update invoice.py

--- Python_Projects/Module1/invoice.py
+++ Python_Projects/Module1/invoice.py
@@ -1,6 +1,4 @@
 print('This is generating Invoice')
-# def my_invoicefunction():
-#   return (print("Hello from my invoice function"))
 class invoice():
     def __init__(self):
         print("The Invoice is ")
