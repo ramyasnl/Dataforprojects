f293feb181e90437babc9099ef41ac903f3e82fb
Ramya S Manivannan <76931568+ramyasnl@users.noreply.github.com>
2022-03-06 14:42:07 -0600

    Create tryinit.py

---
+++ Python_Projects/tryinit.py
@@ -0,0 +1,14 @@
+from Modules1.invoice import invoice
+from Modules1.order import order
+#  input("Enter torder he options "invoice" or "order":)
+print('Enter your  options "invoice" or "order":')
+x = input()
+if (x =="invoice"):
+    f= invoice()
+    print(f)
+    # print("d")
+elif(x =="order"):
+    e = order()
+    print(e)
+else:
+    print("wrong order")
